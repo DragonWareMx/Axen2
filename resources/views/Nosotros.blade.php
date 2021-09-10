e47a1716a388c96270c7d354a0f140b4d1b1ba4d
Dulce G Marín <66979331+DulceGaby@users.noreply.github.com>
2021-09-10 00:37:52 -0500

    la view xd

---
+++ resources/views/Nosotros.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- UIkit CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/css/uikit.min.css" />
+
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+        PAGINA NOSOTROS
+    </body>
+
+</html>
