b3d172ab988c9c3f597e9c1e5bfa3e268359ab62
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-09-10 14:38:08 -0500

    Cambios

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -3,6 +3,13 @@
     <div class="back-right" style="z-index: 4"></div>
     <div class="uk-container uk-container-xlarge uk-padding-larger" style="z-index: 5">
         <div class="title-mapa">Encuentra tú sucursal más cercana</div>
+        <div class="uk-grid-medium uk-margin-large-top" uk-grid>
+            <div class="uk-width-2-3@m">
+                eu miki
+            </div>
+            <div class="uk-width-expand@m">
+                vilemiki
+            </div>
+        </div>
     </div>
-    <div class="uk-grid-medium" uk-grid></div>
 </div>
