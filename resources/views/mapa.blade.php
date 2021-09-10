1c4e379541125f40665c7617ff2d15d1aed8817d
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-09-10 15:29:00 -0500

    Ya solo falta el mapa
    El M.A.P.A.

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -7,8 +7,26 @@
             <div class="uk-width-2-3@m">
                 eu miki
             </div>
-            <div class="uk-width-expand@m">
-                vilemiki
+            <div class="uk-width-expand@m uk-flex uk-flex-center uk-flex-middle">
+                <div class="card-email uk-flex uk-flex-center uk-flex-middle uk-flex-wrap">
+                    <div class="text-contacto">Contacto</div>
+
+                    <div class="label-txta">Nombre:</div>
+                    <textarea class="uk-text-area uk-margin-small-bottom taC" name="" id="nombre" cols="30"
+                        rows="10"></textarea>
+                    <div class="label-txta">Teléfono:</div>
+                    <textarea class="uk-text-area uk-margin-small-bottom taC" name="" id="" cols="30"
+                        rows="10"></textarea>
+                    <div class="label-txta">Producto:</div>
+                    <textarea class="uk-text-area uk-margin-bottom taC" name="" id="" cols="30" rows="10"></textarea>
+
+                    <button class="btn-contacto uk-margin-small-bottom">Enviar</button>
+                    <div class="uk-flex uk-flex-center uk-flex-middle uk-margin-bottom" style="width: 90%">
+                        <label class="politicas-txt"><input class="uk-checkbox check-or" type="checkbox">Acepto las
+                            Políticas de
+                            Privacidad</label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
