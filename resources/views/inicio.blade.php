4b40f2543ad8b89cfb1ec828c01d495ee00c82d7
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-09-10 16:13:12 -0500

    Mapa feo

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -12,28 +12,60 @@ Inicio
 <link
     href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
     rel="stylesheet">
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.maphighlight.min.js"></script>
+<script type="text/javascript">
+    /* State IDS */
+		var states = ['AGU', 'BCN', 'BCS', 'CAM', 'CHH',
+			'CHP', 'COA', 'COL', 'DIF', 'DUR',
+			'GRO', 'GUA', 'HID', 'JAL', 'MEX',
+			'MIC', 'MOR', 'NAY', 'NLE', 'OAX',
+			'PUE', 'QUE', 'ROO', 'SIN', 'SLP',
+			'SON', 'TAB', 'TAM', 'TLA', 'VER',
+			'YUC', 'ZAC'
+		];
+		/* State Names */
+		var state_names = ['Aguascalientes', 'Baja California Norte', 'Baja California Sur', 'Campeche', 'Chihuahua', 'Chiapas', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guerrero', 'Guanajuato', 'Hidalgo', 'Jalisco', 'Edo. Mexico', 'Michoacán', 'Morelos', 'Nayarit', 'Nuevo León', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'Sinaloa', 'San Luis Potosí', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatán', 'Zacatecas'];
+		$(function () {
+			$('.map').maphilight({ fade: false });
+		});
+		$(document).ready(function () {
+			$('.area').hover(function () {
+				var id = $(this).attr('id');
+				var state = $.inArray(id, states);
+
+				$('#edo').html(state_names[state]);
+			});
+		});
+</script>
 @endsection
 
 @section('header')
-    {{-- parte blanca --}}
-    <div class='ini_background' style='background-color:white;padding-top:100px;'>
-        <div class='ini_content uk-container uk-container-large'>
-            <img src={{asset('/img/inicio/poli1.svg')}} class='ini_poli1'>
-            <div style='color:#2E3E58;font-size:100px;font-weight:bold;line-height:1;font-family:poppins;' class='uk-width-1-1'>Que tu dinero</div>
-            <div style='color:#2E3E58;font-size:46px;font-weight:medium;margin-top:0px;' class='uk-width-1-1'>Trabaje mientras descansas</div>
-            <a class='ini_button1'>Más</a>
-        </div>
+{{-- parte blanca --}}
+<div class='ini_background' style='background-color:white;padding-top:100px;'>
+    <div class='ini_content uk-container uk-container-large'>
+        <img src={{asset('/img/inicio/poli1.svg')}} class='ini_poli1'>
+        <div style='color:#2E3E58;font-size:100px;font-weight:bold;line-height:1;font-family:poppins;'
+            class='uk-width-1-1'>Que tu dinero</div>
+        <div style='color:#2E3E58;font-size:46px;font-weight:medium;margin-top:0px;' class='uk-width-1-1'>Trabaje
+            mientras descansas</div>
+        <a class='ini_button1'>Más</a>
     </div>
+</div>
 
-    {{-- parte azul --}}
-    <div class='ini_background' style='background:no-repeat center url("/img/back/inicio2.jpg");background-size:cover;'>
-        <div class='ini_content uk-container uk-container-large'>
-            <img src={{asset('/img/logos/axenblanco.png')}} class='ini_logo'>
-            <div style='color:white;font-size:57px;font-weight:bold;line-height:1;' class='uk-width-1-1'>Sobre nosotros</div>
-            <div style='color:white;font-size:18px;margin-top:10px;' class='uk-width-2-3'>Somos una Empresa Internacional de Tecnología Financiera con productos y servicios especializados.</div>
-            <a class='ini_button2'>Texto</a>
+{{-- parte azul --}}
+<div class='ini_background' style='background:no-repeat center url("/img/back/inicio2.jpg");background-size:cover;'>
+    <div class='ini_content uk-container uk-container-large'>
+        <img src={{asset('/img/logos/axenblanco.png')}} class='ini_logo'>
+        <div style='color:white;font-size:57px;font-weight:bold;line-height:1;' class='uk-width-1-1'>Sobre nosotros
         </div>
+        <div style='color:white;font-size:18px;margin-top:10px;' class='uk-width-2-3'>Somos una Empresa Internacional de
+            Tecnología Financiera con productos y servicios especializados.</div>
+        <a class='ini_button2'>Texto</a>
     </div>
+</div>
 
 
 @endsection
